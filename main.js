f49a22c9eff4c1c0f0d2fd1b88d8abf06243f4f6
BORETS2002 <BORETS2002@example.com>
2022-10-17 18:13:42 +0300

    finish

---
+++ main.js
@@ -0,0 +1,45 @@
+var elInput = document.querySelector(".input")
+var elBtn = document.querySelector(".botn")
+var elfiz = document.querySelector(".fiz")
+var elbuzz = document.querySelector(".buzz")
+var elFizBuz = document.querySelector(".FizzBuzz")
+var non = document.querySelector(".non")
+ 
+
+
+fizAR = []
+buzAR = []
+fizbuzAR = []
+
+
+
+elBtn.addEventListener("click", function (evt) {
+  evt.preventDefault()
+  elInputVal = elInput.value;
+
+  if (elInputVal % 3 == 0 && elInputVal % 5 == 0  ){
+    fizAR.push(elInputVal)
+    buzAR.push(elInputVal)
+    fizbuzAR.push(elInputVal)
+    elfiz.textContent = fizAR;
+    elbuzz.textContent = buzAR;
+    elFizBuz.textContent = fizbuzAR;
+  }
+  else if (elInputVal % 3 == 0){
+    fizAR.push(elInputVal)
+    elfiz.textContent = fizAR;
+  }
+
+  else if (elInputVal % 5 == 0){
+    buzAR.push(elInputVal)
+
+    elbuzz.textContent = buzAR;
+  }
+  else{
+    non.textContent = "bu son fizbaz emas"
+  }
+  elInput.value = ""
+})
+
+
+// elInput.value = "";
